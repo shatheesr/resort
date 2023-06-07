1670c61ca729eed5b04f6fc40a3e18ed200c8512
shatheesr2709 <727821tuec218@skct.edu.in>
2023-06-07 19:12:13 +0530

    first Commit

---
+++ src/compontnts/Routing.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import {Router, createRoot} from "react-router-dom";
+import ContactUs from "./ContactUs";
+import HomePage from "./HomePage";
+import Login from "./Login";
+import SignupPage from "./SignupPage";
+import ProfilePage from './ProfilePage'; 
+import ChangePasswordPage from './ChangePasswordPage';
+import AboutUsPage from './AboutUsPage';
+export default function Routing() {
+  return (
+    <div>
+        <Router>
+            <Routes>
+
+            <Route path="/" element={<Login/>}/>
+            <Route path="/signuppage" element={<SignupPage/>}/>
+            <Route path="/homepage" element={<HomePage/>}/>
+            <Route path="/contactus" element={<ContactUs/>}/>
+            <Route path='/profilepage' element={<ProfilePage/>}/>
+            <Route path='/changepassword' element={<ChangePasswordPage/>}/>
+            <Route path='/aboutus' element={<AboutUsPage/>}/>
+            </Routes>
+        </Router>
+
+    </div>
+  )
+}
